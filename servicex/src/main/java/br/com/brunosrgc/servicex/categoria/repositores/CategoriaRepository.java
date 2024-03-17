75a6b5556ca184d276db187d4b83865509959a8e
Bruno Sérgio <brunosergiocosta5@gmail.com>
2024-03-17 19:27:04 -0300

    Initial commit

---
+++ servicex/src/main/java/br/com/brunosrgc/servicex/categoria/repositores/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package br.com.brunosrgc.servicex.categoria.respositores;
+
+import br.com.brunosrgc.servicex.categoria.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
